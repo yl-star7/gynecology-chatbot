3b84c382ae1064d7288801c54859b646937dec73
강정석 <ubermensch1218@gmail.com>
2026-04-20 20:12:24 +0900

    auto: session end commit

--- scripts/supabase-migrate-startup.sh
+++ scripts/supabase-migrate-startup.sh
@@ -30,7 +30,7 @@ curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc \
   -o /usr/share/postgresql-common/pgdg/apt.postgresql.org.asc
 sh -c 'echo "deb [signed-by=/usr/share/postgresql-common/pgdg/apt.postgresql.org.asc] https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 apt-get update -y
-apt-get install -y postgresql-client-16
+apt-get install -y postgresql-client-17
 
 curl -fsSL -o /usr/local/bin/cloud-sql-proxy \
   https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v2.15.0/cloud-sql-proxy.linux.amd64
